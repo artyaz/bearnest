59005ea0d650c6f1ffbd559d16b207d0dd180b11
“artyaz” <achmilenko58@gmail.com>
2024-04-21 14:06:44 +0300

    Added keys to list elements in footer
    Resolved React's unique "key" prop warning for list elements within the footer component by adding keys based on their index positions. This change ensures that each component in a list is uniquely identifiable, improving rendering performance and predictability, especially during updates. It's crucial for maintaining optimal UI responsiveness and eliminating potential bugs in dynamic lists.

--- src/frontend/components/footer.jsx
+++ src/frontend/components/footer.jsx
@@ -30,19 +30,19 @@ export default function Footer() {
     },
   ];
 
-  const links = linkSectionData.map((data) => (
-    <div className="flex flex-1 flex-col gap-4">
+  const links = linkSectionData.map((data, index) => (
+    <div key={index} className="flex flex-1 flex-col gap-4"> {/* Key added here */}
       <h3 className="leading-[1.25]">{data.title}</h3>
       <ul className="flex flex-col gap-2 text-[16px] font-medium leading-[1.4]">
-        {data.links.map((link) => (
-          <li>
+        {data.links.map((link, subIndex) => (
+          <li key={subIndex}> {/* Key added here using the subIndex */}
             <Link href={"/"}>{link}</Link>
           </li>
         ))}
       </ul>
     </div>
   ));
-
+  
   return (
     <footer className="flex flex-col px-4 font-e-ukraine tablet:px-8 desktop:px-40">
       <section className="flex flex-col gap-24 bg-[url('../../public/images/bear-paw.svg')] bg-contain bg-center bg-no-repeat px-6 py-8 tablet:px-10 tablet:py-8 laptop:bg-auto">
@@ -53,7 +53,7 @@ export default function Footer() {
             width={129}
             height={40}
           />
-
+          
           <div className="flex flex-col items-center gap-1 tablet:items-end desktop:col-start-3">
             <BButton
               text="Request a call"
@@ -65,28 +65,28 @@ export default function Footer() {
               <span>info@logoipsum.com</span>
             </address>
           </div>
-
+  
           <div className="flex flex-wrap gap-6 tablet:col-span-2 desktop:col-start-2 desktop:col-end-3 desktop:row-start-1">
             {links}
           </div>
         </div>
-
+  
         <div className="flex flex-col items-center gap-8 text-[12px] font-medium leading-tight tablet:flex-row tablet:justify-center">
           <div className="flex gap-8 tablet:gap-2">
             <BButton type="rounded" additionalStyles="h-12 w-12" />
             <BButton type="rounded" additionalStyles="h-12 w-12" />
             <BButton type="rounded" additionalStyles="h-12 w-12" />
           </div>
-
+  
           <address className="flex-1 text-center not-italic">
             1 Chome-1-1 Dogenzaka, Shibuya City, Tokyo 150-0043, Japan
           </address>
-
+  
           <p>
             <span className="font-bold">16+</span> / Sales policy
           </p>
         </div>
       </section>
     </footer>
-  );
+  );  
 }
