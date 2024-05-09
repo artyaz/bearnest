537d2d74b4c63e09b4b01ee881c986ff1594b3e8
“artyaz” <achmilenko58@gmail.com>
2024-05-09 21:04:31 +0300

    Refactor page.js to use React hooks
    Migrated data fetching in Entries component to utilize React's useState and useEffect hooks for managing furniture items' data state. This approach enhances the component's responsiveness and maintainability by leveraging React's built-in hooks for better state management and side-effects handling. The refactor also includes cleaner imports and quotation mark consistency, improving code readability and consistency.
    
    - Introduced useState for local data state management.
    - Utilized useEffect for side-effect operations, specifically fetching furniture items asynchronously.
    - Streamlined import statements and standardized to double quotes for consistency.

--- src/app/entries/page.js
+++ src/app/entries/page.js
@@ -1,40 +1,47 @@
-'use client';
+"use client";
+import { useState, useEffect } from "react";
 import { columns } from "./data";
 import { DataTable } from "./data-table";
-import { Card, CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle, } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import FurnitureItem from "@/models/furniture-item";
 
-async function getData() {
-    try {
-      const furnitureItems = await FurnitureItem.fetchLimitedData();
-  
-      console.log(furnitureItems);
-  
-      return furnitureItems;
-    } catch (error) {
+export default function Entries() {
 
-      console.error("Failed to fetch furniture items:", error);
-      return []; 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const furnitureItems = await FurnitureItem.fetchLimitedData();
+
+        console.log(furnitureItems);
+
+        setData(furnitureItems);
+      } catch (error) {
+        console.error("Failed to fetch furniture items:", error);
+        return [];
+      }
     }
-  }
-
-export default async function Entries() {
-  const data = await getData();
+    getData();
+  }, []);
 
   return (
     <div className="container mx-auto py-10">
-        <Card>
-  <CardHeader>
-    <CardTitle>Entries Management</CardTitle>
-  </CardHeader>
-  <CardContent>
-      <DataTable columns={columns} data={data} />
-      </CardContent>
-</Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Entries Management</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
